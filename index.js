56bd497628461c8d11d17619761ed8a1ab1f0779
ALUNO C30 <alunoc30@unifor.local>
2025-04-24 21:37:56 -0300

    Inicialização do projeto

---
+++ index.js
@@ -0,0 +1,13 @@
+app.get('/info', (req, res) => {
+    res.json({ linguagem: 'Node.js', biblioteca: 'Express' });
+    });
+    
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+res.send('Olá, servidor Node.js com Express funcionando!');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Servidor rodando na porta ${PORT}`);
+});
