2a6196d2d59550411801b9a6a7da57422bd49a63
ALUNO C30 <alunoc30@unifor.local>
2025-04-24 21:56:45 -0300

    index

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-app.get('/info', (req, res) => {
-    res.json({ linguagem: 'Node.js', biblioteca: 'Express' });
-    });
-    
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => {
 res.send('Olá, servidor Node.js com Express funcionando!');
 });
+
+app.get('/info', (req, res) => {
+    res.json({ linguagem: 'Node.js', biblioteca: 'Express' });
+    });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log(`Servidor rodando na porta ${PORT}`);
